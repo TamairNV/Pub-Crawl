8704a2c8bc8e4363f309b55ac3a00507d515f191
Tamer Vassib <147373183+TamairNV@users.noreply.github.com>
2026-06-25 00:44:53 +0100

    clean_up.py

--- flask-backend/app/clean_up.py
+++ flask-backend/app/clean_up.py
@@ -1,13 +1,25 @@
 
 
 from SQL_manager import get_all_photos
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = '../../uploads'
 
 all_photos = get_all_photos()
-print(all_photos)
+
 
 import os
 
 footage = [f for f in os.listdir(os.path.abspath(UPLOAD_FOLDER))]
+
+
+all_photos = [i['id'].split('/')[-1] for i in all_photos]
+counter = 0
+print(all_photos)
+print("----------------\n\n\n\n\n")
 print(footage)
+for photo in footage:
+  if photo not in all_photos:
+    os.remove(UPLOAD_FOLDER+"/photo")
+    counter +=1
+
+print(f"{counter}: removed files")
 
