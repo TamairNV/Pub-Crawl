428097cb5c6644c5547960c6c8a9b88b9a62ba67
Tamer Vassib <147373183+TamairNV@users.noreply.github.com>
2026-06-25 00:45:46 +0100

    clean_up.py

--- flask-backend/app/clean_up.py
+++ flask-backend/app/clean_up.py
@@ -18,7 +18,7 @@ print("----------------\n\n\n\n\n")
 print(footage)
 for photo in footage:
   if photo not in all_photos:
-    os.remove(UPLOAD_FOLDER+"/photo")
+    os.remove(UPLOAD_FOLDER+f"/{photo}")
     counter +=1
 
 print(f"{counter}: removed files")
